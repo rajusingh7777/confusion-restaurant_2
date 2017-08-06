7c84e17ba3290d6e8f01a4b826ebd2a02860f657
Raju Singh <singh.raj@husky.neu.edu>
2017-08-06 10:07:21 -0400

    Angular- part2, SPA aboutus completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Dish } from '../shared/dish'
-import { DishService } from '../services/dish.service'
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 
-import { Promotion } from '../shared/promotion'
-import { PromotionService } from '../services/promotion.service'
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
+
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 
 @Component({
@@ -16,12 +19,16 @@ export class HomeComponent implements OnInit {
 
   dish: Dish;
   promotion: Promotion;
+  leader: Leader;
 
-  constructor(private dishservice: DishService, private promotionservice: PromotionService) { }
+  constructor(private dishservice: DishService,
+     private promotionservice: PromotionService,
+     private leaderservice: LeaderService ) { }
 
   ngOnInit() {
     this.dish = this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionservice.getFeaturedPromotion()
+    this.promotion = this.promotionservice.getFeaturedPromotion();
+    this.leader = this.leaderservice.getFeaturedLeader();
   }
 
 }
